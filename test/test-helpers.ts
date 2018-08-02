812d3bb70e95c3fd81b796afe30eec5738fdaafa
Mike North <michael.l.north@gmail.com>
2018-08-02 12:20:02 -0700

    fix: Make each log count assertion (error, log, debug, warn) optional

--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -29,27 +29,35 @@ export function logCountAssert(
     message,
     assert,
     printer
-  }: { message: string; assert: Assert;  printer: Printer; },
-  { e, w, l, d }: { e: number; w: number; l: number; d: number }
+  }: { message: string; assert: Assert; printer: Printer },
+  { e, w, l, d }: { e?: number; w?: number; l?: number; d?: number }
 ) {
-  assert.equal(
-    (printer as any).messages.error.length,
-    e,
-    `${message}: ${e} error(s) were logged`
-  );
-  assert.equal(
-    (printer as any).messages.warn.length,
-    w,
-    `${message}: ${w} warning(s) was logged`
-  );
-  assert.equal(
-    (printer as any).messages.debug.length,
-    d,
-    `${message}: ${d} debug(s) were logged`
-  );
-  assert.equal(
-    (printer as any).messages.log.length,
-    l,
-    `${message}: ${l} log(s) were logged`
-  );
+  if (typeof e !== 'undefined') {
+    assert.equal(
+      (printer as any).messages.error.length,
+      e,
+      `${message}: ${e} error(s) were logged`
+    );
+  }
+  if (typeof w !== 'undefined') {
+    assert.equal(
+      (printer as any).messages.warn.length,
+      w,
+      `${message}: ${w} warning(s) was logged`
+    );
+  }
+  if (typeof d !== 'undefined') {
+    assert.equal(
+      (printer as any).messages.debug.length,
+      d,
+      `${message}: ${d} debug(s) were logged`
+    );
+  }
+  if (typeof l !== 'undefined') {
+    assert.equal(
+      (printer as any).messages.log.length,
+      l,
+      `${message}: ${l} log(s) were logged`
+    );
+  }
 }
