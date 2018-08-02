8fba2b37fbbcbb0c002933c9c5b9a8530a62bfd9
Mike North <michael.l.north@gmail.com>
2018-08-02 12:11:16 -0700

    feat: tagged logging

---
+++ test/tagged-logging-test.ts
@@ -0,0 +1,34 @@
+import Logger from 'bite-log';
+import { logCountAssert, makeTestPrinter } from './test-helpers';
+
+QUnit.module('Tagged logging');
+
+QUnit.test(
+  'Pushing a tag results in the log mesages being prefixed',
+  assert => {
+    const printer = makeTestPrinter();
+    const logger = new Logger(4, printer);
+    logger.log('hello');          // "hello"
+    logger.log('world');          // "world"
+    logger.pushPrefix('foo');
+    logger.log('goodbye');        // "[foo]: goodbye"
+    logger.log('mars');           // "[foo]: mars"
+    logger.pushPrefix('bar');
+    logger.log('(and saturn!)');  // "[foo][bar]: (and saturn)!"
+    logger.popPrefix();
+    logger.popPrefix();
+    logger.log('but not pluto');  // "but not pluto"
+    logCountAssert(
+      { message: 'after logging four things', assert, printer },
+      { l: 6 }
+    );
+    const logs = printer.messages.log;
+    assert.ok(logs[0].join('').indexOf('foo') < 0, 'First message has no "foo" tag');
+    assert.ok(logs[1].join('').indexOf('foo') < 0, 'Second message has no "foo" tag');
+    assert.ok(logs[2].join('').indexOf('foo') >= 0, 'Third message has a "foo" tag');
+    assert.ok(logs[3].join('').indexOf('foo') >= 0, 'Fourth message has a "foo" tag');
+    assert.ok(logs[4].join('').indexOf('foo') >= 0, 'Fifth message has a "foo" tag');
+    assert.ok(logs[4].join('').indexOf('bar') >= 0, 'Fifth message has a "bar" tag');
+    assert.ok(logs[5].join('').indexOf('foo') < 0, 'Sixth message has no "foo" tag');
+  }
+);
--- test/test-helpers.ts
+++ test/test-helpers.ts
@@ -29,35 +29,35 @@ export function logCountAssert(
     message,
     assert,
     printer
-  }: { message: string; assert: Assert; printer: Printer },
+  }: { message: string; assert: Assert;  printer: Printer; },
   { e, w, l, d }: { e?: number; w?: number; l?: number; d?: number }
 ) {
   if (typeof e !== 'undefined') {
-    assert.equal(
-      (printer as any).messages.error.length,
-      e,
-      `${message}: ${e} error(s) were logged`
-    );
+  assert.equal(
+    (printer as any).messages.error.length,
+    e,
+    `${message}: ${e} error(s) were logged`
+  );
   }
   if (typeof w !== 'undefined') {
-    assert.equal(
-      (printer as any).messages.warn.length,
-      w,
-      `${message}: ${w} warning(s) was logged`
-    );
+  assert.equal(
+    (printer as any).messages.warn.length,
+    w,
+    `${message}: ${w} warning(s) was logged`
+  );
   }
   if (typeof d !== 'undefined') {
-    assert.equal(
-      (printer as any).messages.debug.length,
-      d,
-      `${message}: ${d} debug(s) were logged`
-    );
+  assert.equal(
+    (printer as any).messages.debug.length,
+    d,
+    `${message}: ${d} debug(s) were logged`
+  );
   }
   if (typeof l !== 'undefined') {
-    assert.equal(
-      (printer as any).messages.log.length,
-      l,
-      `${message}: ${l} log(s) were logged`
-    );
+  assert.equal(
+    (printer as any).messages.log.length,
+    l,
+    `${message}: ${l} log(s) were logged`
+  );
   }
 }
