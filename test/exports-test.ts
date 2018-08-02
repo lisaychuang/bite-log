3e190d0c47096c6f7c16e2a9769b3a3ad8214ee1
Lisa Huang <lisa.yc.huang@gmail.com>
2018-08-01 23:33:10 -0700

    Added public API export test

---
+++ test/exports-test.ts
@@ -0,0 +1,7 @@
+import * as allExports from 'bite-log';
+
+QUnit.module('the right things are exported from bite-log');
+
+QUnit.test('Logger exists', assert => {
+    assert.ok(allExports.default, 'default export exists');
+});
