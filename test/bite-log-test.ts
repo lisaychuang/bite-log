222accc957d1725ffffbcc1f2eff17159c0e6dd0
Lisa Huang <lisa.yc.huang@gmail.com>
2018-08-01 23:30:13 -0700

    Added Printer tests with two levels

--- test/bite-log-test.ts
+++ test/bite-log-test.ts
@@ -1,13 +1,21 @@
-import Logger from 'bite-log';
+// import Logger from 'bite-log';
 
-QUnit.module('bite-log tests');
+// QUnit.module('bite-log tests');
 
-QUnit.test('Logger', assert => {
-  const logger = new Logger(2); // only warns and error
-  // console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
-  logger.debug(' (debug) Oh my heavens');
-  logger.log(' (log) Oh my heavens');
-  logger.warn(' (warn) Oh my heavens');
-  logger.error(' (error) Oh my heavens');
-  assert.ok(logger, 'logger exists');
-});
+// QUnit.test('Logger', assert => {
+//   const logger = new Logger(2); // only warns and error
+//   // console.log('%c Oh my heavens! ', 'background: #222; color: #bada55');
+//   logger.debug(' (debug) Oh my heavens');
+//   logger.log(' (log) Oh my heavens');
+//   logger.warn(' (warn) Oh my heavens');
+//   logger.error(' (error) Oh my heavens');
+//   assert.ok(logger, 'logger exists');
+// });
+
+
+// logger
+
+// logger
+//   .bgWhite.red('my message')
+//   .blue.large('another');
+//   .debug();
--- test/logging-test.ts
+++ test/logging-test.ts
@@ -6,7 +6,7 @@ function makeTestPrinter(): Printer & { messages: any } {
             log: [],
             debug: [],
             warn: [],
-            error: [],
+            error: []
         },
         log(msg: string) {
             printer.messages.log.push(msg);
@@ -24,12 +24,56 @@ function makeTestPrinter(): Printer & { messages: any } {
     return printer;
 }
 
+function logCountAssert(
+    {message, assert, logger, printer}: { message: string; assert: Assert; logger: Logger; printer: Printer },
+    { e, w, l, d }: { e: number; w: number; l: number; d: number }
+) {
+    assert.equal(
+        (printer as any).messages.error.length,
+        e,
+        `${message}: ${e} error(s) were logged`
+    );
+    assert.equal(
+        (printer as any).messages.warn.length,
+        w,
+        `${message}: ${w} warning(s) was logged`
+    );
+    assert.equal(
+        (printer as any).messages.debug.length,
+        d,
+        `${message}: ${d} debug(s) were logged`
+    );
+    assert.equal(
+        (printer as any).messages.log.length,
+        l,
+        `${message}: ${l} log(s) were logged`
+    );
+}
+
 QUnit.module('Logging through the printer');
 
-QUnit.test('Logger', assert => {
+QUnit.test('Logger @ level 4', assert => {
+    const printer = makeTestPrinter();
+    const logger = new Logger(4, printer); // only warns and error
+    logger.warn('a warning');
+    logCountAssert({ message: 'after a warning', assert, logger, printer }, { e: 0, w: 1, l: 0, d: 0 });
+    logger.error('an error');
+    logCountAssert({ message: 'after an error', assert, logger, printer }, { e: 1, w: 1, l: 0, d: 0 });
+    logger.log('a log');
+    logCountAssert({ message: 'after a log', assert, logger, printer }, { e: 1, w: 1, l: 1, d: 0 });
+    logger.debug('a debug');
+    logCountAssert({ message: 'after a debug', assert, logger, printer }, { e: 1, w: 1, l: 1, d: 1 });
+});
+
+QUnit.test('Logger @ level 2', assert => {
     const printer = makeTestPrinter();
     const logger = new Logger(2, printer); // only warns and error
     logger.warn('a warning');
-
-    assert.equal(printer.messages.warn.length, 1, 'One warning was logged');
+    logCountAssert({ message: 'after logging a warning', assert, logger, printer }, { e: 0, w: 1, l: 0, d: 0 });
+    logger.error('an error');
+    logCountAssert({ message: 'after logging an error', assert, logger, printer }, { e: 1, w: 1, l: 0, d: 0 });
+    logger.log('a log');
+    logCountAssert({ message: 'after logging', assert, logger, printer }, { e: 1, w: 1, l: 0, d: 0 });
+    logger.debug('a debug');
+    logCountAssert({ message: 'after logging a debug', assert, logger, printer }, { e: 1, w: 1, l: 0, d: 0});
 });
